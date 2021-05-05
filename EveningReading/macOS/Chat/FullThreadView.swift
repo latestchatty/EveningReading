1b05c04196aa4c7faf16320e7822acca86eeead7
cjhodge <19308384+cjhodge@users.noreply.github.com>
2021-05-05 06:28:14 -0600

    macOS collapse thread
    macOS collapse thread

--- EveningReading/macOS/Chat/FullThreadView.swift
+++ EveningReading/macOS/Chat/FullThreadView.swift
@@ -23,6 +23,9 @@ struct FullThreadView: View {
     @State private var lols = [String: Int]()
     @State private var lolTypeCount: Int = 0
     
+    @State private var isThreadCollapsed: Bool = false
+    @State private var showingCollapseAlert: Bool = false
+    
     private func getThreadData() {
         if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] != nil
         {
@@ -64,56 +67,68 @@ struct FullThreadView: View {
     }
 
     var body: some View {
-        VStack (alignment: .leading) {
-            
-            ThreadCategoryColor[self.rootPostCategory].frame(height: 5)
-            
-            HStack {
-                AuthorNameView(name: self.$rootPostAuthor, postId: self.$threadId)
-                
-                ContributedView(contributed: self.$contributed)
-                
-                if self.hasLols {
-                    LolView(lols: self.$lols)
-                }
-                Spacer()
-                
-                TimeRemainingIndicator(percent: .constant(self.rootPostDate.getTimeRemaining()))
-                    .frame(width: 10, height: 10)
-                
-                Text(self.rootPostDate.getTimeAgo())
-                    .font(.body)
-                
-                Image(systemName: "eye.slash")
-                    .imageScale(.large)
-                    .onTapGesture(count: 1) {
+        VStack {
+            if !self.isThreadCollapsed {
+                VStack (alignment: .leading) {
+                    
+                    ThreadCategoryColor[self.rootPostCategory].frame(height: 5)
+                    
+                    HStack {
+                        AuthorNameView(name: self.$rootPostAuthor, postId: self.$threadId)
+                        
+                        ContributedView(contributed: self.$contributed)
+                        
+                        if self.hasLols {
+                            LolView(lols: self.$lols)
+                        }
+                        Spacer()
+                        
+                        TimeRemainingIndicator(percent: .constant(self.rootPostDate.getTimeRemaining()))
+                            .frame(width: 10, height: 10)
+                        
+                        Text(self.rootPostDate.getTimeAgo())
+                            .font(.body)
+                        
+                        Image(systemName: "eye.slash")
+                            .imageScale(.large)
+                            .onTapGesture(count: 1) {
+                                self.showingCollapseAlert.toggle()
+                            }
+                        
+                        Image(systemName: "tag")
+                            .imageScale(.large)
+                            .onTapGesture(count: 1) {
+                            }
+                        
+                        Image(systemName: "arrowshape.turn.up.left")
+                            .imageScale(.large)
+                            .onTapGesture(count: 1) {
+                            }
                     }
-                
-                Image(systemName: "tag")
-                    .imageScale(.large)
-                    .onTapGesture(count: 1) {
+                    .padding(.leading, 20)
+                    .padding(.trailing, 20)
+                    
+                    HStack {
+                        Text("\(self.rootPostBody)")
+                            .font(.body)
                     }
-                
-                Image(systemName: "arrowshape.turn.up.left")
-                    .imageScale(.large)
-                    .onTapGesture(count: 1) {
-                    }
-            }
-            .padding(.leading, 20)
-            .padding(.trailing, 20)
-            
-            HStack {
-                Text("\(self.rootPostBody)")
-                    .font(.body)
-            }
-            .padding(.init(top: 0, leading: 20, bottom: 15, trailing: 20))
+                    .padding(.init(top: 0, leading: 20, bottom: 15, trailing: 20))
 
+                }
+                .onAppear(perform: getThreadData)
+                .frame(maxWidth: .infinity)
+                .background(Color("ThreadBubblePrimary"))
+                .cornerRadius(10)
+                .padding(.init(top: 0, leading: 20, bottom: 10, trailing: 20))
+            } else {
+                Spacer().frame(height: 8)
+            }
         }
-        .onAppear(perform: getThreadData)
-        .frame(maxWidth: .infinity)
-        .background(Color("ThreadBubblePrimary"))
-        .cornerRadius(10)
-        .padding(.init(top: 0, leading: 20, bottom: 10, trailing: 20))
+        .alert(isPresented: self.$showingCollapseAlert) {
+            Alert(title: Text("Hide Thread?"), message: Text(""), primaryButton: .cancel(), secondaryButton: Alert.Button.default(Text("OK"), action: {
+                self.isThreadCollapsed = true
+            }))
+        }
     }
 }
 
