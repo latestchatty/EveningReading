dd3ab11dc4710695c850ababd21fde79d95be3ce
cjhodge <19308384+cjhodge@users.noreply.github.com>
2021-05-08 00:01:44 -0600

    removed silly refresh notification
    removed silly refresh notification

--- EveningReading/macOS/Chat/macOSChatView.swift
+++ EveningReading/macOS/Chat/macOSChatView.swift
@@ -11,9 +11,6 @@ struct macOSChatView: View {
     @EnvironmentObject var appSessionStore: AppSessionStore
     @EnvironmentObject var chatStore: ChatStore
     
-    @State var notificationCount: Int = 0
-    @State var showingRefreshNotice: Bool = false
-    
     private func fetchChat() {
         if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] != nil || chatStore.threads.count > 0
         {
@@ -52,16 +49,6 @@ struct macOSChatView: View {
                 }
             }
             .onAppear(perform: fetchChat)
-        }
-        .overlay(RefreshNoticeView(showingNotice: self.$showingRefreshNotice))
-        .onReceive(chatStore.$loadingChat) { loading in
-            // Don't show notification at launch
-            if !loading && self.notificationCount > 2 {
-                withAnimation {
-                    self.showingRefreshNotice = true
-                }
-            }
-            self.notificationCount += 1
         }
         .frame(maxHeight: .infinity)
         .navigationTitle("Chat")
