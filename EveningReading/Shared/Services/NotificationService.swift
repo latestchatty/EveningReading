d2fb0a32c0bdacc7d06c55dd712cd17a96b49f9a
cjhodge <19308384+cjhodge@users.noreply.github.com>
2021-05-17 08:25:22 -0600

    register user before registering for push notifictions for a device
    register user before registering for push notifictions for a device

--- EveningReading/Shared/Services/NotificationService.swift
+++ EveningReading/Shared/Services/NotificationService.swift
@@ -8,7 +8,11 @@
 import Foundation
 import SwiftUI
 
-struct RegisterReponse {
+struct RegisterUserReponse {
+    var status: Int
+}
+
+struct RegisterDeviceReponse {
     var status: Int
 }
 
@@ -20,16 +24,52 @@ class NotificationService {
         self.session = session
         self.decoder = decoder
     }
+
+    public func registerUser(clientId: String, handler: @escaping (Result<RegisterUserReponse, Error>) -> Void) {
+        guard
+            var urlComponents = URLComponents(string: "https://www.woggle.net/lcappnotification/change_new.php")
+            else { preconditionFailure("Can't create url components...") }
+
+        urlComponents.queryItems = [
+         URLQueryItem(name: "action", value: "add"),
+         URLQueryItem(name: "type", value: "user"),
+         URLQueryItem(name: "getvanity", value: "1"),
+         URLQueryItem(name: "getreplies", value: "1"),
+         URLQueryItem(name: "user", value: clientId)
+        ]
+        
+        guard
+            let url = urlComponents.url
+            else { preconditionFailure("Can't create url from url components...") }
+        
+        let urlSession: URLSession = .shared
+        
+        urlSession.dataTask(with: url) { [weak self] data, _, error in
+            if let error = error {
+                print("push register user error \(error)")
+                //handler(.failure(error))
+            } else {
+                do {
+                    let data = data ?? Data()
+                    print("push register user success")
+                    print("\(data)")
+                    handler(.success(RegisterUserReponse(status: 1)))
+                } catch {
+                    handler(.failure(error))
+                    print("push register user error \(error)")
+                }
+            }
+        }.resume()
+    }
     
-    public func register(deviceUUID: String, deviceTokenClean: String, deviceName: String, deviceModel: String, deviceVersion: String, clientId: String, appName: String, appVersion: String, handler: @escaping (Result<RegisterReponse, Error>) -> Void) {
-        let task = "register"
+    public func registerDevice(deviceUUID: String, deviceTokenClean: String, deviceName: String, deviceModel: String, deviceVersion: String, clientId: String, appName: String, appVersion: String, handler: @escaping (Result<RegisterDeviceReponse, Error>) -> Void) {
 
         guard
             var urlComponents = URLComponents(string: "https://www.woggle.net/lcappnotification/apns_new.php")
             else { preconditionFailure("Can't create url components...") }
                  
         urlComponents.queryItems = [
-         URLQueryItem(name: "task", value: task),
+         URLQueryItem(name: "task", value: "register"),
          URLQueryItem(name: "appname", value: appName),
          URLQueryItem(name: "appversion", value: appVersion),
          URLQueryItem(name: "deviceuid", value: deviceUUID),
@@ -48,6 +88,8 @@ class NotificationService {
             else { preconditionFailure("Can't create url from url components...") }
              
         let urlSession: URLSession = .shared
+        
+        // print("\(url)")
 
         urlSession.dataTask(with: url) { [weak self] data, _, error in
             if let error = error {
@@ -58,7 +100,7 @@ class NotificationService {
                     let data = data ?? Data()
                     print("push register success")
                     print("\(data)")
-                    handler(.success(RegisterReponse(status: 1)))
+                    handler(.success(RegisterDeviceReponse(status: 1)))
                 } catch {
                     handler(.failure(error))
                     print("push register error \(error)")
@@ -75,8 +117,9 @@ class NotificationStore: ObservableObject {
         self.service = service
     }
     
-    @Published private(set) var registerResponse: RegisterReponse = RegisterReponse(status: 0)
-        
+    @Published private(set) var registerUserResponse: RegisterUserReponse = RegisterUserReponse(status: 0)
+    @Published private(set) var registerDeviceResponse: RegisterDeviceReponse = RegisterDeviceReponse(status: 0)
+    
     func register() {
         let username: String? = KeychainWrapper.standard.string(forKey: "Username")
         let defaults = UserDefaults.standard
@@ -102,18 +145,31 @@ class NotificationStore: ObservableObject {
                     appVersion = version
                 }
                 
-                print("registering for push")
-                service.register(deviceUUID: deviceUUID, deviceTokenClean: deviceTokenClean, deviceName: deviceName, deviceModel: deviceModel, deviceVersion: deviceVersion, clientId: clientIdClean, appName: appName, appVersion: appVersion) { [weak self] result in
+                print("registering user for push")
+                service.registerUser(clientId: clientIdClean) { [weak self] result in
                     DispatchQueue.main.async {
                         switch result {
                         case .success(let response):
-                            self?.registerResponse = response
+                            self?.registerUserResponse = response
                         case .failure:
-                            self?.registerResponse = RegisterReponse(status: 0)
+                            self?.registerUserResponse = RegisterUserReponse(status: 0)
                         }
                     }
                 }
-                
+
+                print("registering device for push")
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5)) {
+                    self.service.registerDevice(deviceUUID: deviceUUID, deviceTokenClean: deviceTokenClean, deviceName: deviceName, deviceModel: deviceModel, deviceVersion: deviceVersion, clientId: clientIdClean, appName: appName, appVersion: appVersion) { [weak self] result in
+                        DispatchQueue.main.async {
+                            switch result {
+                            case .success(let response):
+                                self?.registerDeviceResponse = response
+                            case .failure:
+                                self?.registerDeviceResponse = RegisterDeviceReponse(status: 0)
+                            }
+                        }
+                    }
+                }
             }
         }
     }
