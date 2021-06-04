d42053723133bb560670044901c26eb4e4001e26
cjhodge <19308384+cjhodge@users.noreply.github.com>
2021-06-04 08:31:47 -0600

    cleaned up TagsWebView
    cleaned up TagsWebView

--- EveningReading/iOS/Tags/TagsView.swift
+++ EveningReading/iOS/Tags/TagsView.swift
@@ -8,19 +8,12 @@
 import SwiftUI
 
 struct TagsView: View {
-    @ObservedObject var tagsWebViewStore = TagsWebViewStore()
     @State private var webViewProgress: Double = 0
     @State private var webViewLoading: Bool = true
     @State private var hyperlinkUrl: String = "about:blank"
+    @State private var goToPostId: Int = 0
+    @State private var showingPost: Bool = false
     
-    func goBack() {
-        tagsWebViewStore.webView.goBack()
-    }
-
-    func goForward() {
-        tagsWebViewStore.webView.goForward()
-    }
-
     var body: some View {
         VStack {
             //GoToPostView()
@@ -29,24 +22,16 @@ struct TagsView: View {
                     .progressViewStyle(LinearProgressViewStyle(tint: Color(UIColor.systemBlue)))
                     .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
             }
-            TagsWebView(webView: self.tagsWebViewStore.webView, viewModel: self.tagsWebViewStore, estimatedProgress: self.$webViewProgress, isLoading: self.$webViewLoading, loadUrl: self.$hyperlinkUrl)
+            
+            // Push ThreadDetailView
+            NavigationLink(destination: ThreadDetailView(threadId: .constant(0), postId: self.$goToPostId, replyCount: .constant(-1), isSearchResult: .constant(true)), isActive: self.$showingPost) {
+                EmptyView()
+            }.isDetailLink(false).hidden().allowsHitTesting(false)
+            
+            TagsWebView()
         }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
-                let user: String? = KeychainWrapper.standard.string(forKey: "Username")
-                let pass: String? = KeychainWrapper.standard.string(forKey: "Password")
-                self.tagsWebViewStore.loadUrlWithShackAuth(urlStr: "https://www.shacknews.com/tags-user", username: user ?? "", password: pass ?? "")
-            }
-        }
-        /*
-        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
-                let user: String? = KeychainWrapper.standard.string(forKey: "Username")
-                let pass: String? = KeychainWrapper.standard.string(forKey: "Password")
-                self.tagsWebViewStore.loadUrlWithShackAuth(urlStr: "https://www.shacknews.com/tags-user", username: user ?? "", password: pass ?? "")
-            }
-        }
-        */
+
+        // View settings
         .background(Color("PrimaryBackground").frame(height: 2600).offset(y: -80))
         .edgesIgnoringSafeArea(.bottom)
         .navigationViewStyle(StackNavigationViewStyle())
--- EveningReading/iOS/Tags/TagsWebView.swift
+++ EveningReading/iOS/Tags/TagsWebView.swift
@@ -1,255 +1,37 @@
 //
-//  TagsWebView.swift
-//  EveningReading
+//  TagsWebView2.swift
+//  EveningReading (iOS)
 //
-//  Created by Chris Hodge on 5/8/21.
+//  Created by Chris Hodge on 6/4/21.
 //
 
+import Foundation
 import SwiftUI
 import Combine
 import WebKit
-import os
+import UIKit
 
-public class TagsWebViewStore: ObservableObject {
-    @Published public var webView: WKWebView {
-        didSet {
-            setupObservers()
-        }
-    }
-    @Published var contentHeight: CGFloat
+struct TagsWebView: UIViewRepresentable {
+    var webView: WKWebView?
 
-    public init(webView: WKWebView = WKWebView(), contentHeight: CGFloat = 288.0) {
-        self.webView = webView
-        self.contentHeight = contentHeight
-        setupObservers()
+    init() {
+        let processPool = WKProcessPool()
+        let configuration = WKWebViewConfiguration()
+        configuration.websiteDataStore = WKWebsiteDataStore.nonPersistent()
+        configuration.processPool = processPool
+        self.webView = WKWebView(frame: CGRect.zero, configuration: configuration)
     }
-  
-    private func setupObservers() {
-        func subscriber<Value>(for keyPath: KeyPath<WKWebView, Value>) -> NSKeyValueObservation {
-            return webView.observe(keyPath, options: [.prior]) { _, change in
-                if change.isPrior {
-                    self.objectWillChange.send()
-                }
-            }
-        }
-        
-        observers = [
-            subscriber(for: \.title),
-            subscriber(for: \.url),
-            subscriber(for: \.isLoading),
-            subscriber(for: \.estimatedProgress),
-            subscriber(for: \.hasOnlySecureContent),
-            subscriber(for: \.serverTrust),
-            subscriber(for: \.canGoBack),
-            subscriber(for: \.canGoForward)
-        ]
-    }
-    
-    public func loadUrlWithShackAuth(urlStr: String, username: String, password: String) {
-                
-        if username == "" || password == "" {
-            self.webView = WKWebView(frame: CGRect.zero)
-            let req = URLRequest(url: URL(string: urlStr)!)
-            self.webView.load(req)
-            return
-        }
-        
-        DispatchQueue.main.async {
-            let processPool = WKProcessPool()
-            let configuration = WKWebViewConfiguration()
-            configuration.websiteDataStore = WKWebsiteDataStore.nonPersistent()
-            configuration.processPool = processPool
-            self.webView = WKWebView(frame: CGRect.zero, configuration: configuration)
-            
-            let req = URLRequest(url: URL(string: urlStr)!)
-            
-            var liCookie = ""
-            var intCookie = ""
-            let newPostUrl = URL(string: "https://www.shacknews.com/account/signin")!
-            var components = URLComponents(url: newPostUrl, resolvingAgainstBaseURL: false)!
-            components.queryItems = [
-                URLQueryItem(name: "user-identifier", value: username),
-                URLQueryItem(name: "supplied-pass", value: password)
-            ]
-            let query = components.url!.query
 
-            var cookieRequest = URLRequest(url: newPostUrl)
-            cookieRequest.httpMethod = "POST"
-            cookieRequest.httpBody = Data(query!.utf8)
-
-            let session = URLSession.shared
-            let task = session.dataTask(with: cookieRequest as URLRequest, completionHandler: { data, response, error in
-                guard error == nil else {
-                    return
-                }
-                guard let _ = data else {
-                    return
-                }
-                
-                let cookieName = "_shack_li_"
-                if let cookie = HTTPCookieStorage.shared.cookies?.first(where: { $0.name == cookieName }) {
-                    liCookie = cookie.value
-                }
-                let cookieName2 = "_shack_int_"
-                if let cookie = HTTPCookieStorage.shared.cookies?.first(where: { $0.name == cookieName2 }) {
-                    intCookie = cookie.value
-                }
-                
-                DispatchQueue.main.async {
-                    let cookieLI = HTTPCookie(properties: [
-                        .domain: "www.shacknews.com",
-                        .path: "/",
-                        .name: "_shack_li_",
-                        .value: liCookie,
-                        .secure: "TRUE",
-                        .expires: NSDate(timeIntervalSinceNow: 31556926)
-                    ])!
-                    let cookieINT = HTTPCookie(properties: [
-                        .domain: "www.shacknews.com",
-                        .path: "/",
-                        .name: "_shack_int_",
-                        .value: intCookie,
-                        .secure: "TRUE",
-                        .expires: NSDate(timeIntervalSinceNow: 604800)
-                    ])!
-                    
-                    /*
-                    let task = URLSession.shared.dataTask(with: URL(string: urlStr)!) { data, response, error in
-                        if let error = error {
-                            print("TagsWebView error")
-                            return
-                        }
-                        guard let httpResponse = response as? HTTPURLResponse,
-                            (200...299).contains(httpResponse.statusCode) else {
-                            print("TagsWebView server error")
-                            return
-                        }
-                        if let mimeType = httpResponse.mimeType, mimeType == "text/html",
-                            let data = data,
-                            let string = String(data: data, encoding: .utf8) {
-                            DispatchQueue.main.async {
-                                let removedTargets = string.replacingOccurrences(of: #"target="_blank""#, with: "")
-                                self.webView.loadHTMLString(removedTargets, baseURL: URL(string: "https://www.shacknews.com"))
-                            }
-                        }
-                    }
-                    task.resume()
-                    */
-                    
-                    
-                    self.webView.configuration.websiteDataStore.httpCookieStore.setCookie(cookieLI) {
-                        self.webView.configuration.websiteDataStore.httpCookieStore.setCookie(cookieINT) {
-                            var cookieLength = 0
-                            self.webView.configuration.websiteDataStore.fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { (records) in
-                                for record in records {
-                                    cookieLength += record.displayName.count
-                                }
-                            }
-                            
-                            self.webView.load(req)
-                        }
-                    }
-                    
-                    
-                }
-            })
-            task.resume()
-        }
-    }
-  
-    private var observers: [NSKeyValueObservation] = []
-  
-    deinit {
-        observers.forEach {
-            $0.invalidate()
-        }
-    }
-}
-
-public struct TagsWebView: View, UIViewRepresentable {
-    public let webView: WKWebView
-    
-    private var viewModel: TagsWebViewStore
-    @Binding var estimatedProgress: Double
-    @Binding var isLoading: Bool
-    @Binding var loadUrl: String
-
-    public typealias UIViewType = TagViewContainerView<WKWebView>
-  
-    public init(webView: WKWebView, viewModel: TagsWebViewStore, estimatedProgress: Binding<Double>, isLoading: Binding<Bool>, loadUrl: Binding<String>) {
-        self.webView = webView
-        self.viewModel = TagsWebViewStore()
-        self._estimatedProgress = estimatedProgress
-        self._isLoading = isLoading
-        self._loadUrl = loadUrl
-    }
-  
-    public func makeUIView(context: UIViewRepresentableContext<TagsWebView>) -> TagsWebView.UIViewType {
-        self.webView.navigationDelegate = context.coordinator
-        return TagViewContainerView()
-    }
-  
-    public func updateUIView(_ uiView: TagsWebView.UIViewType, context: UIViewRepresentableContext<TagsWebView>) {
-        
-        if uiView.contentView !== webView {
-            DispatchQueue.main.async {
-                uiView.contentView = webView
-                uiView.contentView?.navigationDelegate = context.coordinator
-            }
-        }
-        
-        /*
-        if webView.url?.description == "about:blank" {
-            let req = URLRequest(url: URL(string: loadUrl)!)
-            webView.load(req)
-        }
-        */
-        
-        if self.webView.estimatedProgress >= 1.0 {
-            DispatchQueue.main.async {
-                self.estimatedProgress = 1.0
-            }
-        } else {
-            DispatchQueue.main.async {
-                self.estimatedProgress = self.webView.estimatedProgress
-            }
-        }
-        
-        if !self.webView.isLoading {
-            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(200)) {
-                self.isLoading = false
-            }
-        } else {
-            DispatchQueue.main.async {
-                self.isLoading = true
-            }
-        }
-        
-    }
-    
-    public func makeCoordinator() -> TagsWebView.Coordinator {
-        Coordinator(self)
-    }
-  
-    public class Coordinator: NSObject, WKNavigationDelegate {
+    class Coordinator: NSObject, WKUIDelegate, WKNavigationDelegate {
         var parent: TagsWebView
-    
+
         init(_ parent: TagsWebView) {
             self.parent = parent
         }
-    
-        public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            
-        }
-        
-        private func webViewWebContentProcessDidTerminate() {
-            
-        }
-        
+
         public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
             switch navigationAction.navigationType {
             case .linkActivated:
-                print("linkActivated")
                 if let url = navigationAction.request.url {
                     if url.absoluteString.contains("chatty?id") {
                         let shared = UIApplication.shared
@@ -264,32 +46,112 @@ public struct TagsWebView: View, UIViewRepresentable {
                     decisionHandler(.allow)
                 }
             default:
-                print("otherAction")
                 decisionHandler(.allow)
             }
         }
-        
-    }
-}
 
-public class TagViewContainerView<ContentView: UIView>: UIView {
-    var contentView: ContentView? {
-        willSet {
-            contentView?.removeFromSuperview()
+        func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+            //
         }
-        didSet {
-            if let contentView = contentView {
-                addSubview(contentView)
-                contentView.backgroundColor = UIColor.systemBackground
-                contentView.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> WKWebView  {
+        let tagsUrl = "https://www.shacknews.com/tags-user"
+        let username: String = KeychainWrapper.standard.string(forKey: "Username") ?? ""
+        let password: String = KeychainWrapper.standard.string(forKey: "Password") ?? ""
+        
+        if username != "" && password != "" {
+                    
+            DispatchQueue.main.async {
+                let req = URLRequest(url: URL(string: tagsUrl)!)
                 
-                NSLayoutConstraint.activate([
-                    contentView.leadingAnchor.constraint(equalTo: leadingAnchor),
-                    contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
-                    contentView.topAnchor.constraint(equalTo: topAnchor),
-                    contentView.bottomAnchor.constraint(equalTo: bottomAnchor)
-                ])
+                var liCookie = ""
+                var intCookie = ""
+                let newPostUrl = URL(string: "https://www.shacknews.com/account/signin")!
+                var components = URLComponents(url: newPostUrl, resolvingAgainstBaseURL: false)!
+                components.queryItems = [
+                    URLQueryItem(name: "user-identifier", value: username),
+                    URLQueryItem(name: "supplied-pass", value: password)
+                ]
+                let query = components.url!.query
+
+                var cookieRequest = URLRequest(url: newPostUrl)
+                cookieRequest.httpMethod = "POST"
+                cookieRequest.httpBody = Data(query!.utf8)
+
+                let session = URLSession.shared
+                let task = session.dataTask(with: cookieRequest as URLRequest, completionHandler: { data, response, error in
+                    guard error == nil else {
+                        return
+                    }
+                    guard let _ = data else {
+                        return
+                    }
+                    
+                    let cookieName = "_shack_li_"
+                    if let cookie = HTTPCookieStorage.shared.cookies?.first(where: { $0.name == cookieName }) {
+                        liCookie = cookie.value
+                    }
+                    let cookieName2 = "_shack_int_"
+                    if let cookie = HTTPCookieStorage.shared.cookies?.first(where: { $0.name == cookieName2 }) {
+                        intCookie = cookie.value
+                    }
+                    
+                    DispatchQueue.main.async {
+                        let cookieLI = HTTPCookie(properties: [
+                            .domain: "www.shacknews.com",
+                            .path: "/",
+                            .name: "_shack_li_",
+                            .value: liCookie,
+                            .secure: "TRUE",
+                            .expires: NSDate(timeIntervalSinceNow: 31556926)
+                        ])!
+                        let cookieINT = HTTPCookie(properties: [
+                            .domain: "www.shacknews.com",
+                            .path: "/",
+                            .name: "_shack_int_",
+                            .value: intCookie,
+                            .secure: "TRUE",
+                            .expires: NSDate(timeIntervalSinceNow: 604800)
+                        ])!
+                        
+                        self.webView?.configuration.websiteDataStore.httpCookieStore.setCookie(cookieLI) {
+                            self.webView?.configuration.websiteDataStore.httpCookieStore.setCookie(cookieINT) {
+                                var cookieLength = 0
+                                self.webView?.configuration.websiteDataStore.fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { (records) in
+                                    for record in records {
+                                        cookieLength += record.displayName.count
+                                    }
+                                }
+                                
+                                self.webView?.load(req)
+                            }
+                        }
+                        
+                        
+                    }
+                })
+                task.resume()
             }
+            
+            return webView!
         }
+        
+        DispatchQueue.main.async {
+            let url = URL(string: tagsUrl)!
+            let request = URLRequest(url: url)
+            webView?.load(request)
+        }
+        
+        return webView!
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.navigationDelegate = context.coordinator
+        uiView.uiDelegate = context.coordinator
     }
 }
