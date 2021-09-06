5679bde1560e83fe792707fdc6ec0e24a07d52ff
Willie Zutz <boarder2@bit-shift.com>
2021-09-05 23:17:52 -0600

    Padding for inline compose.

--- EveningReading/macOS/Chat/macOSPostExpandedView.swift
+++ EveningReading/macOS/Chat/macOSPostExpandedView.swift
@@ -102,6 +102,7 @@ struct macOSPostExpandedView: View {
                     }
                     if (showReply) {
                         macOSComposeView(postId: self.postId)
+                            .padding(.top, 8)
                     }
                     
                 }
