15c560f0db459111f275e0d937ac75541fcaa77f
cjhodge <19308384+cjhodge@users.noreply.github.com>
2022-06-08 08:37:50 -0600

    search
    search

--- EveningReading/iPhone/Chat/ChatView.swift
+++ EveningReading/iPhone/Chat/ChatView.swift
@@ -17,6 +17,10 @@ struct ChatView: View {
 
     @State private var isGettingChat: Bool = false
     @State private var noInternet: Bool = false
+    @State private var showingSearch: Bool = false
+    @State private var searchTerms: String = ""
+    @State private var searchPadding: Double = -100
+    @State private var resultsPadding: Double = 1
 
     //@State private var isPushNotificationAlertShowing: Bool = false
     
@@ -25,53 +29,76 @@ struct ChatView: View {
         {
             return Array(chatData.threads)
         }
-        let threads = chatStore.threads.filter({ return self.appSessionStore.threadFilters.contains($0.posts.filter({ return $0.parentId == 0 })[0].category) && !self.appSessionStore.collapsedThreads.contains($0.posts.filter({ return $0.parentId == 0 })[0].threadId)})
+        var threads = chatStore.threads
+        
+        if searchTerms != "" {
+            threads = chatStore.threads.filter({ return
+                $0.posts.filter({ return $0.parentId == 0  })[0].body.lowercased().contains(searchTerms.lowercased()) &&
+            self.appSessionStore.threadFilters.contains($0.posts.filter({ return $0.parentId == 0 })[0].category) &&
+            !self.appSessionStore.collapsedThreads.contains($0.posts.filter({ return $0.parentId == 0 })[0].threadId)})
+        } else {
+            threads = chatStore.threads.filter({ return
+            self.appSessionStore.threadFilters.contains($0.posts.filter({ return $0.parentId == 0 })[0].category) &&
+            !self.appSessionStore.collapsedThreads.contains($0.posts.filter({ return $0.parentId == 0 })[0].threadId)})
+        }
         return Array(threads)
     }
     
     var body: some View {
-        VStack {
-            
-            // Comment out to preview
-            //GoToPostView(currentViewName: "ChatView")
-            
-            // height: 70
-            RefreshableScrollView(height: 20, refreshing: self.$chatStore.gettingChat, scrollTarget: self.$chatStore.scrollTargetChat, scrollTargetTop: self.$chatStore.scrollTargetChatTop) {
+        ZStack {
+            VStack {
+                // Comment out to preview
+                //GoToPostView(currentViewName: "ChatView")
                 
-                // No Internet/Data
-                if self.noInternet {
-                    Text("Oops! No Data.")
-                        .foregroundColor(Color("NoData"))
-                        .padding(5)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color("NoDataLabel"), lineWidth: 1)
-                        )
-                        .padding(.top, 20)
+                // height: 70
+                RefreshableScrollView(height: 20, refreshing: self.$chatStore.gettingChat, scrollTarget: self.$chatStore.scrollTargetChat, scrollTargetTop: self.$chatStore.scrollTargetChatTop) {
+                    
+                    // No Internet/Data
+                    if self.noInternet {
+                        Text("Oops! No Data.")
+                            .foregroundColor(Color("NoData"))
+                            .padding(5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color("NoDataLabel"), lineWidth: 1)
+                            )
+                            .padding(.top, 20)
+                    }
+                    
+                    // Scroll to top
+                    VStack {
+                        Spacer().frame(maxWidth: .infinity).frame(height: self.resultsPadding)
+                    }.id(9999999999991)
+                    
+                    // All non-hidden threads
+                    ForEach(filteredThreads(), id: \.threadId) { thread in
+                        ThreadRow(threadId: .constant(thread.threadId), activeThreadId: .constant(0))
+                            .padding(.bottom, -20)
+                            .id(thread.threadId)
+                    }
+                    
+                    // Scroll to bottom / padding
+                    VStack {
+                        Spacer().frame(maxWidth: .infinity).frame(height: 30)
+                    }.id(9999999999993)
+                    
                 }
-                
-                // Scroll to top
-                VStack {
-                    Spacer().frame(maxWidth: .infinity).frame(height: 1)
-                }.id(9999999999991)
-                
-                // All non-hidden threads
-                ForEach(filteredThreads(), id: \.threadId) { thread in
-                    ThreadRow(threadId: .constant(thread.threadId), activeThreadId: .constant(0))
-                        .padding(.bottom, -20)
-                        .id(thread.threadId)
-                }
-                
-                // Scroll to bottom / padding
-                VStack {
-                    Spacer().frame(maxWidth: .infinity).frame(height: 30)
-                }.id(9999999999993)
-                
+                //.overlay(PushNotificationView(isAlertShowing: self.$isPushNotificationAlertShowing))
             }
-            //.overlay(PushNotificationView(isAlertShowing: self.$isPushNotificationAlertShowing))
-
+            
+            if showingSearch {
+                VStack {
+                    TextField("Search for...", text: $searchTerms)
+                        .padding()
+                        .autocapitalization(.none)
+                        .background(Color(UIColor.systemBackground))
+                        .cornerRadius(4.0)
+                        .padding(EdgeInsets(top: self.searchPadding, leading: 20, bottom: 0, trailing: 20))
+                        .shadow(color: Color("HomeButtonShadow"), radius: 10, x: 0, y: 10)
+                    Spacer()
+                }
+            }
         }
-        
         // View settings
         .background(Color("PrimaryBackground").frame(height: 2600).offset(y: -80))
         .edgesIgnoringSafeArea(.bottom)
@@ -79,7 +106,28 @@ struct ChatView: View {
         .navigationBarTitle("Chat", displayMode: .inline)
         
         // New thread button
-        .navigationBarItems(leading: Spacer().frame(width: 26, height: 16), trailing: ComposePostView(isRootPost: true))
+        .navigationBarItems(leading: Spacer().frame(width: 26, height: 16), trailing: HStack {
+                // Search
+                Button(action: {
+                    self.showingSearch = !self.showingSearch
+                    withAnimation {
+                        if self.showingSearch {
+                            self.searchPadding = 10
+                            self.resultsPadding = 70
+                        } else {
+                            self.searchPadding = -100
+                            self.searchTerms = ""
+                            self.resultsPadding = 1
+                        }
+                    }
+                }) {
+                Image(systemName: "magnifyingglass.circle")
+                    .imageScale(.large)
+                    .foregroundColor(self.colorScheme == .dark ? Color(UIColor.white) : Color(UIColor.systemBlue))
+                }
+                // Compose
+                ComposePostView(isRootPost: true)
+            })
 
         // If refreshing thread after posting
         .overlay(LoadingView(show: self.$isGettingChat, title: .constant("")))
