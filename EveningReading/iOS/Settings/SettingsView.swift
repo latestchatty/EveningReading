ff748a8ab208e0741aa1e4d075b996ccc50b39e2
cjhodge <19308384+cjhodge@users.noreply.github.com>
2023-04-10 13:13:14 -0600

    sync watch iphone only
    sync watch iphone only

--- EveningReading/iOS/Settings/SettingsView.swift
+++ EveningReading/iOS/Settings/SettingsView.swift
@@ -27,14 +27,16 @@ struct SettingsView: View {
             }
             Section(header: Text("ACCOUNT")) {
                 AccountView()
-                NavigationLink(destination: SyncWatchView()) {
-                    HStack {
-                        Text("Sync With Watch")
-                        Spacer()
-                        Image("chevron.right")
-                    }
-                    .contentShape(Rectangle())
-                }.isDetailLink(false)
+                if UIDevice.current.userInterfaceIdiom == .phone {
+                    NavigationLink(destination: SyncWatchView()) {
+                        HStack {
+                            Text("Sync With Watch")
+                            Spacer()
+                            Image("chevron.right")
+                        }
+                        .contentShape(Rectangle())
+                    }.isDetailLink(false)
+                }
             }
             Section(header: Text("FILTERS")) {
                 CategoriesView()
